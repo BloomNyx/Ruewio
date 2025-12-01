e522f0d86fb983a43dcbd249de06e1eb754eee7d
BloomNyx <bagsuhyeon507@gmail.com>
2025-12-01 10:28:18 +0900

    ruewio

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+// C:\Users\mhncity\ruewio\vite.config.js
+
+import { defineConfig } from 'vite';
+
+// 설정을 포함하는 객체를 기본 내보내기(export default) 해야 합니다.
+export default defineConfig({
+  // 여기에 필요한 Vite 구성 옵션을 추가합니다.
+  // 예: plugins: [...], base: '/', build: { ... }
+});
