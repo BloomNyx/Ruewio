ad233524bf56f4dd003fe33a592c0eca59ac2414
BloomNyx <bagsuhyeon507@gmail.com>
2025-12-01 09:52:51 +0900

    ruewio

--- src/assers/js/main.js
+++ src/assers/js/main.js
@@ -1,24 +1,11 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import {menu} from "./menu";
+import {port} from "./port";
+import {link} from "./link";
+import {smooth} from "./smooth";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+window.addEventListener("load" ,  function(){
+  smooth();
+  link();
+  menu();
+  port();
+})
