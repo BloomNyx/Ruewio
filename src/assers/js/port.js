cfca086368df92042dd0d25ebb7220ecdd1a7d71
BloomNyx <bagsuhyeon507@gmail.com>
2025-12-03 10:04:41 +0900

    ruewio

--- src/assers/js/port.js
+++ src/assers/js/port.js
@@ -3,14 +3,14 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 
 export function port() {
     gsap.registerPlugin(ScrollTrigger);
-    const horSection = gsap.utils.toArray(".port_item");
+    const horSection = gsap.utils.toArray(".port__item");
 
     gsap.to(horSection, {
-        xPercent: -120 * (horSection.length -1),
+        xPercent: -120 * (horSection.length - 1),
         ease: "none",
         scrollTrigger: {
             trigger: "#port",
-            start:"top 56px",
+            start: "top 56px",
             end: "+=3000",
             pin: true,
             scrub: 1,
