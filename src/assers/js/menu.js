fbb9c0ad7e71aa7ad31ecd1ea2fafa8bde01effc
BloomNyx <bagsuhyeon507@gmail.com>
2025-12-05 10:11:19 +0900

    ruewio

--- src/assers/js/menu.js
+++ src/assers/js/menu.js
@@ -1,11 +1,13 @@
-const headerToggle = document.getElementById("headerToggle");
-const headerNav = document.querySelector(".headerNav");
+export function menu() {
+    const headerToggle = document.getElementById("headerToggle");
+    const headerNav = document.querySelector(".headerNav");
 
-if(headerToggle) {
-    headerToggle.addEventListener("click", () => {
-        headerNav.classList.toggle("show");
-        headerToggle.getAttribute("aria-expanded") === "true"
-        ? headerToggle.setAttribute("aira-expanded", "false")
-        : headerToggle.setAttribute("aria-expanded", "true");
-    })
+    if (headerToggle) {
+        headerToggle.addEventListener("click", () => {
+            headerNav.classList.toggle("show");
+            headerToggle.getAttribute("aria-expanded") === "true"
+                ? headerToggle.setAttribute("aria-expanded", "false")
+                : headerToggle.setAttribute("aria-expanded", "true");
+        });
+    }
 }
