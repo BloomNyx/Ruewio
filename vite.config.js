10196c38df7c503cf22bb2196d0020908b348656
BloomNyx <bagsuhyeon507@gmail.com>
2025-12-05 10:18:16 +0900

    ruewio

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/ruewio/',   // ⭐ 저장소 이름 그대로 넣기
+  base: '/Ruewio/',   // ⭐ 저장소 이름 그대로 넣기
   plugins: [react()],
 });
